6f919768fed679deeb9fbb0166d46a6d9be0eb7b
cvvergara <vicky@erosion.dev>
2026-06-25 15:40:44 -0600

    Vehicles chapter work

--- docs/scripts/basic/vehicles/images.sql
+++ docs/scripts/basic/vehicles/images.sql
@@ -1,35 +1,5 @@
 
-CREATE OR REPLACE VIEW vehicle_route_going_png AS
-WITH dijkstra AS (
-SELECT * FROM pgr_dijkstra(
-  'SELECT id, source, target, cost, reverse_cost
-   FROM vehicle_net',
-  @ID_1@, @ID_3@,
-  directed := true)
-)
-SELECT seq, start_vid, end_vid, geom FROM dijkstra JOIN vehicle_net ON(edge = id);
 
-
-CREATE OR REPLACE VIEW vehicle_route_coming_png AS
-WITH dijkstra AS (
-SELECT * FROM pgr_dijkstra(
-  'SELECT id, source, target, cost, reverse_cost
-  FROM vehicle_net',
-  @ID_3@, @ID_1@,
-  directed := true)
-)
-SELECT seq, start_vid, end_vid, geom FROM dijkstra JOIN vehicle_net ON(edge = id);
-
-CREATE OR REPLACE VIEW vehicle_time_is_money_png AS
-WITH dijkstra AS (
-SELECT * FROM pgr_dijkstra(
-  'SELECT id, source, target,
-    cost / 3600 * 100 AS cost,
-    reverse_cost / 3600 * 100 AS reverse_cost
-   FROM taxi_net',
-  @ID_3@, @ID_1@)
-)
-SELECT seq, start_vid, end_vid, geom FROM dijkstra JOIN taxi_net ON(edge = id);
 
 UPDATE configuration SET penalty=1;
 
@@ -51,6 +21,9 @@ WHERE tag_key IN ('cycleway') OR tag_value IN ('cycleway');
 
 -- Penalizing with 5 times the costs the unknown
 UPDATE configuration SET penalty=5 WHERE tag_value IN ('unclassified');
+
+-- Penalizing with 3 times the costs of residential roads
+UPDATE configuration SET penalty=5 WHERE tag_value IN ('residential');
 
 -- Encuraging the use of "fast" roads
 UPDATE configuration SET penalty=0.5 WHERE tag_value IN ('tertiary');
@@ -79,17 +52,17 @@ SELECT * FROM pgr_dijkstra(
   ' SELECT id, source, target, cost_s * penalty AS cost, reverse_cost_s * penalty AS reverse_cost
   FROM ways JOIN configuration
   USING (tag_id) ',
-ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_5@],
-ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_5@])
+ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_3@],
+ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_3@])
 )
 SELECT seq,
   CASE WHEN start_vid = @ID_1@ THEN '@PLACE_1@' WHEN start_vid = @ID_2@ THEN '@PLACE_2@'
   WHEN start_vid = @ID_3@ THEN '@PLACE_3@' WHEN start_vid = @ID_4@ THEN '@PLACE_4@'
-  WHEN start_vid = @ID_5@ THEN '@PLACE_5@' END
+  WHEN start_vid = @ID_3@ THEN '@PLACE_5@' END
   ||','||
   CASE WHEN end_vid = @ID_1@ THEN '@PLACE_1@' WHEN end_vid = @ID_2@ THEN '@PLACE_2@'
   WHEN end_vid = @ID_3@ THEN '@PLACE_3@' WHEN end_vid = @ID_4@ THEN '@PLACE_4@'
-  WHEN end_vid = @ID_5@ THEN '@PLACE_5@' END
+  WHEN end_vid = @ID_3@ THEN '@PLACE_5@' END
   AS name,
   start_vid, end_vid, geom AS geom
 FROM dijkstra JOIN ways ON(edge = id);
@@ -98,17 +71,17 @@ CREATE OR REPLACE VIEW vehicle_no_penalty_routes AS
 WITH dijkstra AS (
 SELECT * FROM pgr_dijkstra(
   ' SELECT id, source, target, cost_s AS cost, reverse_cost_s AS reverse_cost FROM ways ',
-ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_5@],
-ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_5@])
+ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_3@],
+ARRAY[@ID_1@,@ID_2@,@ID_3@,@ID_4@, @ID_3@])
 )
 SELECT seq,
   CASE WHEN start_vid = @ID_1@ THEN '@PLACE_1@' WHEN start_vid = @ID_2@ THEN '@PLACE_2@'
   WHEN start_vid = @ID_3@ THEN '@PLACE_3@' WHEN start_vid = @ID_4@ THEN '@PLACE_4@'
-  WHEN start_vid = @ID_5@ THEN '@PLACE_5@' END
+  WHEN start_vid = @ID_3@ THEN '@PLACE_5@' END
   ||','||
   CASE WHEN end_vid = @ID_1@ THEN '@PLACE_1@' WHEN end_vid = @ID_2@ THEN '@PLACE_2@'
   WHEN end_vid = @ID_3@ THEN '@PLACE_3@' WHEN end_vid = @ID_4@ THEN '@PLACE_4@'
-  WHEN end_vid = @ID_5@ THEN '@PLACE_5@' END
+  WHEN end_vid = @ID_3@ THEN '@PLACE_5@' END
   AS name,
   start_vid, end_vid, geom AS geom
 FROM dijkstra JOIN ways ON(edge = id);
