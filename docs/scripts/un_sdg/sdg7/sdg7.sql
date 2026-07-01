9b3f5cb9af7a9cbdc63c423ccb0e40e8a829ae11
cvvergara <vicky@erosion.dev>
2026-07-01 10:33:38 -0600

    sdg7: adjusting code to tables without prefix

--- docs/scripts/un_sdg/sdg7/sdg7.sql
+++ docs/scripts/un_sdg/sdg7/sdg7.sql
@@ -5,28 +5,28 @@ SHOW search_path;
 \o revert_changes.txt
 
 DROP TABLE IF EXISTS roads.roads_vertices;
-ALTER TABLE roads.roads_ways DROP component;
+ALTER TABLE roads.ways DROP component;
 
 \o only_connected1.txt
 
 SELECT * INTO roads.roads_vertices
 FROM pgr_extractVertices(
   'SELECT id, source, target
-  FROM roads.roads_ways ORDER BY id');
+  FROM roads.ways ORDER BY id');
 
 \o only_connected2.txt
 
 UPDATE roads_vertices v SET geom = ST_startPoint(w.geom)
-FROM roads_ways w WHERE source = v.id;
+FROM roads.ways w WHERE source = v.id;
 
 UPDATE roads_vertices v SET geom = ST_endPoint(w.geom)
-FROM roads_ways w WHERE v.geom IS NULL AND target = v.id;
+FROM roads.ways w WHERE v.geom IS NULL AND target = v.id;
 
 UPDATE roads_vertices set (x,y) = (ST_X(geom), ST_Y(geom));
 
 \o only_connected3.txt
 
-ALTER TABLE roads_ways ADD COLUMN component BIGINT;
+ALTER TABLE roads.ways ADD COLUMN component BIGINT;
 ALTER TABLE roads_vertices ADD COLUMN component BIGINT;
 
 \o only_connected4.txt
@@ -34,30 +34,30 @@ ALTER TABLE roads_vertices ADD COLUMN component BIGINT;
 UPDATE roads_vertices SET component = c.component
 FROM (
   SELECT * FROM pgr_connectedComponents(
-  'SELECT id, source, target, cost, reverse_cost FROM roads_ways')
+  'SELECT id, source, target, cost, reverse_cost FROM roads.ways')
 ) AS c
 WHERE id = node;
 
 \o only_connected5.txt
 
-UPDATE roads_ways SET component = v.component
+UPDATE roads.ways SET component = v.component
 FROM (SELECT id, component FROM roads_vertices) AS v
 WHERE source = v.id;
 
 \o only_connected6.txt
 
 WITH
-all_components AS (SELECT component, count(*) FROM roads_ways GROUP BY component),
+all_components AS (SELECT component, count(*) FROM roads.ways GROUP BY component),
 max_component AS (SELECT max(count) from all_components)
 SELECT component FROM all_components WHERE count = (SELECT max FROM max_component);
 
 \o only_connected7.txt
 
 WITH
-all_components AS (SELECT component, count(*) FROM roads_ways GROUP BY component),
+all_components AS (SELECT component, count(*) FROM roads.ways GROUP BY component),
 max_component AS (SELECT max(count) from all_components),
 the_component AS (SELECT component FROM all_components WHERE count = (SELECT max FROM max_component))
-DELETE FROM roads_ways WHERE component != (SELECT component FROM the_component);
+DELETE FROM roads.ways WHERE component != (SELECT component FROM the_component);
 
 \o only_connected8.txt
 
@@ -71,7 +71,7 @@ DELETE FROM roads_vertices WHERE component != (SELECT component FROM the_compone
 
 PREPARE edges AS
 SELECT id, source, target, length_m AS cost
-FROM roads.roads_ways;
+FROM roads.ways;
 
 \o exercise_10-2.txt
 
@@ -86,6 +86,6 @@ FROM pgr_kruskalDFS('edges', 91);
 \o exercise_12.txt
 
 SELECT sum(length_m)/1000 AS total_km
-FROM roads_ways;
+FROM roads.ways;
 
 \o
